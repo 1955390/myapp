ec99feec80edc119186e48a3c43b2fa1d5b90259
Prem Mohan <premmohan966@gmail.com>
2025-05-06 11:15:00 +0530

    Update Prem_Mohan_learning_app.py.py

--- Prem_Mohan_learning_app.py.py
+++ Prem_Mohan_learning_app.py.py
@@ -7,7 +7,7 @@ import bcrypt
 from PIL import Image
 #from googleapiclient.discovery import build
 import random
-#import smtplib
+import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
